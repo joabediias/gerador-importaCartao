85ed7d0df5723970ef3e6e9c1c353778bead68e1
Joabe Dias <joabemilhomens@hotmail.com>
2026-04-22 13:52:55 -0300

    feature: adicionado novas validações nos campos de parametros

--- app/domain/validators.py
+++ app/domain/validators.py
@@ -20,3 +20,50 @@ def ensure_columns(df: pd.DataFrame, required: Iterable[str] = REQUIRED_COLUMNS)
 def ensure_not_empty(df: pd.DataFrame) -> None:
     if df.empty:
         raise ValidationError("A aba TAXAS não possui linhas preenchidas.")
+
+
+def validate_app_params(params) -> None:
+    if params.tipo_vencimento_parcelas == "U":
+        if params.tipo_inicio_periodo_vencimento != "V":
+            raise ValidationError(
+                "Quando o tipo de vencimento das parcelas for 'Utiliza dias informados pelo usuário', "
+                "o tipo da data inicial do período de vencimento deve ser 'Data da venda'."
+            )
+
+        if params.dia_inicio_periodo_vencimento != 0:
+            raise ValidationError(
+                "Quando o tipo de vencimento das parcelas for 'Utiliza dias informados pelo usuário', "
+                "a data inicial do período de vencimento deve ser 0."
+            )
+
+        if params.tipo_vencimento_primeira_parc != "M":
+            raise ValidationError(
+                "Quando o tipo de vencimento das parcelas for 'Utiliza dias informados pelo usuário', "
+                "o tipo de vencimento da primeira parcela deve ser 'Proximo mes'."
+            )
+
+        if params.dias_para_venc_primeira_parc != 0:
+            raise ValidationError(
+                "Quando o tipo de vencimento das parcelas for 'Utiliza dias informados pelo usuário', "
+                "o dia para vencimento da primeira parcela deve ser 0."
+            )
+
+    if params.tipo_inicio_periodo_vencimento == "V" and params.dia_inicio_periodo_vencimento != 0:
+        raise ValidationError(
+            "Se o tipo da data inicial do período de vencimento for 'Data da venda', o dia deve ser 0."
+        )
+
+    if params.tipo_inicio_periodo_vencimento == "D" and params.dia_inicio_periodo_vencimento <= 0:
+        raise ValidationError(
+            "Se o tipo da data inicial do período de vencimento for 'Dia do mes', informe um valor maior que 0."
+        )
+
+    if params.tipo_vencimento_primeira_parc == "M" and params.dias_para_venc_primeira_parc != 0:
+        raise ValidationError(
+            "Se o tipo de vencimento da primeira parcela for 'Proximo mes', o dia deve ser 0."
+        )
+
+    if params.tipo_vencimento_primeira_parc == "D" and params.dias_para_venc_primeira_parc <= 0:
+        raise ValidationError(
+            "Se o tipo de vencimento da primeira parcela for 'Qtd. de dias', informe um valor maior que 0."
+        )
