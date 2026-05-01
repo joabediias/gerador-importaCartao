6a60cdca0e8f54d187730de9acd6b8c3e0c6b73f
Joabe Dias <joabemilhomens@hotmail.com>
2026-05-01 14:55:05 -0300

    bugfix: ajustado para icon check.svg aparecer no executavel com--onefile

--- app/utils/resource_path.py
+++ app/utils/resource_path.py
@@ -6,12 +6,18 @@ from pathlib import Path
 
 def resource_path(relative_path: str) -> str:
     """
-    Retorna o caminho absoluto de um recurso tanto no modo dev
-    quanto no executável gerado pelo PyInstaller --onedir.
+    Retorna o caminho absoluto do recurso no modo normal,
+    no PyInstaller --onedir e no PyInstaller --onefile.
     """
     if getattr(sys, "frozen", False):
-        base_path = Path(sys.executable).parent / "_internal"
+        # PyInstaller --onefile
+        if hasattr(sys, "_MEIPASS"):
+            base_path = Path(sys._MEIPASS)
+        else:
+            # PyInstaller --onedir
+            base_path = Path(sys.executable).parent / "_internal"
     else:
+        # desenvolvimento
         base_path = Path.cwd()
 
     return (base_path / relative_path).as_posix()
